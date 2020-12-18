cdf360c7a8894489f2783c57a1e13c72ffe8c525
Ramses <faraonramses97@outlook.com>
2020-12-17 19:45:24 -0600

    Primer commit

---
+++ useCuonter.js
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export const useCuonter =  ( initialState = 10 ) => {
+
+    const [counter, setCounter] = useState(initialState);
+
+    const increment = () => {
+        setCounter( counter + 1 );
+    }
+
+    const decrement = ( factor = 1 ) => {
+        setCounter( counter - 1 );
+    }
+
+    const reset = () => {
+        setCounter( initialState );
+    }
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset
+    }
+
+}
